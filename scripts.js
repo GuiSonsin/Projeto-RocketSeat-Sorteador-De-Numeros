1e53d2f06121538b960646fb0c0c588da782bafb
GuiSonsin <sonsinguilherme@gmail.com>
2025-01-31 11:56:35 -0300

    Criando a função de exibir o resultado

--- scripts.js
+++ scripts.js
@@ -50,3 +50,38 @@ if(repeatNumberValue){
     })  
 }
 
+function showResults(numbers){
+    const resultContainer = document.getElementById("result-numbers")
+    // to clear the result in the square 
+    resultContainer.innerHTML = ""
+
+    // creating a square to each number
+    numbers.forEach((number) => {
+        const square = document.createElement("div")
+        square.className = "number-square"
+
+        const rotatingSquare = document.createElement("div")
+        rotatingSquare.className = "rotating-square"
+
+        const numberElement = document.createElement("div")
+        numberElement.className = "number"
+        numberElement.textContent = number
+
+        square.appendChild(rotatingSquare)
+        square.appendChild(numberElement)
+        resultContainer.appendChild(square)
+
+        setTimeout(() => {
+            rotatingSquare.style.animation = "rotate 1s ease-in-out"
+
+            setTimeout(() => {
+                rotatingSquare.style.opacity = "0"
+                numberElement.style.opacity = "1"
+                numberElement.style.transform = "scale(1)"
+            }, 500)
+        })
+        
+    })
+    document.getElementById("form-container").style.display = "none"
+    document.getElementById("result").style.display = "block"
+}
